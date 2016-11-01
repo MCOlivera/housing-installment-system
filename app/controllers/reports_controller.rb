7fa5fb22dbf9f76032f98324794af87424d897fe
Mervin Olivera <mervin.c.olivera@gmail.com>
2016-11-01 16:17:06 +0000

    Added monthly report functionality

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,5 +6,14 @@ class ReportsController < ApplicationController
         else
             @input_year = params[:input_year].to_s
         end
+        
+        if params[:input_month].nil?
+            @input_date = @input_year.to_s + '-12-31'
+        else
+            @input_month = params[:input_month].to_s
+            @input_date = @input_year + '-' + @input_month + '-31'
+        end
+        
+        
     end
 end
